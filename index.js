b05239ba175172b76d44267ef65ef00aad737383
Timmy Le <le.timmy94@gmail.com>
2020-03-11 13:04:34 -0400

    edits

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var myName = 'Timmy';
-myName = myName.toUpperCase();
-
-prompt('What is your name?');
+var name = prompt('What is your name?');
+var firstChar = name.slice(0, 1);
+var uppCaseFirstChar = firstChar.toUpperCase();
